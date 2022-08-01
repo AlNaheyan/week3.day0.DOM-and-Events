b4372172af4deebf4452a2f5d9a5e8e751316ab5
Al Naheyan <aln@seoscholars.org>
2022-08-01 12:15:50 -0400

    Summary

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,10 @@
+let divRight = document.querySelector('.right_side');
+
+divRight.style.backgroundColor = '#d3c26f';
+
+let headings = document.getElementsByClassName('name');
+
+for (let i = 0; i < headings.length; i++) {
+    headings[i].style.textShadow = '-6px 5px 2px yellow';
+}
+
---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,5 @@
+let headings = document.getElementById('item_name');
+headings.innerHTML = 'Frostfang';
+
+let image = document.getElementById('item_img');
+image.src = 'assets/Frostfang_item.png';
